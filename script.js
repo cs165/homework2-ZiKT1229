8162bc0b3846ca204531e9c3ba83c9103d2e23ed
KT_Zheng <a6163508@gmail.com>
2019-04-15 16:33:26 +0800

    增加完成測驗，結果顯示及重新測驗

--- script.js
+++ script.js
@@ -2,16 +2,24 @@
 
 // You can access the RESULTS_MAP from "constants.js" in this file since
 // "constants.js" has been included before "script.js" in index.html.
+const scrollTop = document.getElementsByClassName("question-name")[0];
 const choiceGrids = document.getElementsByClassName("choice-grid");
+const result = document.getElementsByClassName("result")[0];
+const resultH1 = document.getElementsByClassName("result-h1")[0];
+const resultP = document.getElementsByClassName("result-p")[0];
+const restart = document.getElementsByClassName("result-btn")[0];
 const record = {
   one: false,
   two: false,
   three: false
 };
-const choiceGridsClone = [];
+const map = {
+  one: "",
+  two: "",
+  three: ""
+}
 
 Array.from(choiceGrids).forEach((choiceGrid, index) => {
-  choiceGridsClone[index] = choiceGrid.cloneNode(true);
   choiceGrid.addEventListener("click", function clickEvent(event) {
     let finished = Object.values(record).every(bool => bool);
     if (event.target === choiceGrid || finished) return;
@@ -34,12 +42,33 @@ Array.from(choiceGrids).forEach((choiceGrid, index) => {
     currentChoice.classList.toggle("unchosen");
     currentChoice.childNodes[3].src = "images/checked.png";
     record[currentChoice.dataset.questionId] = true;
+    map[currentChoice.dataset.questionId] = currentChoice.dataset.choiceId;
 
     finished = Object.values(record).every(bool => bool);
     if (finished) render();
   });
 });
 
+restart.addEventListener("click", () => {
+  const allChoice = document.getElementsByClassName("choice");
+  Array.from(allChoice).forEach((choice) => {
+    choice.classList.toggle("selected", false);
+    choice.classList.toggle("unchosen", false);
+  });
+
+  record.one = record.two = record.three = false;
+  map.one = map.two = map.three = "";
+  result.classList.add("hide");
+  scrollTop.scrollIntoView({behavior: "smooth"});
+});
+
 function render() {
-  console.log("test");
+  if (map.two === map.three) {
+    resultH1.textContent = `You got: ${RESULTS_MAP[map.two].title}`;
+    resultP.textContent = RESULTS_MAP[map.two].contents;
+  } else {
+    resultH1.textContent = `You got: ${RESULTS_MAP[map.one].title}`;
+    resultP.textContent = RESULTS_MAP[map.one].contents;
+  }
+  result.classList.remove("hide");
 }
