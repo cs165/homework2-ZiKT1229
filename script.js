100a715390219784ab7945f4027305b1ed931393
KT_Zheng <a6163508@gmail.com>
2019-04-15 11:41:14 +0800

    增加 click event

--- script.js
+++ script.js
@@ -2,3 +2,44 @@
 
 // You can access the RESULTS_MAP from "constants.js" in this file since
 // "constants.js" has been included before "script.js" in index.html.
+const choiceGrids = document.getElementsByClassName("choice-grid");
+const record = {
+  one: false,
+  two: false,
+  three: false
+};
+const choiceGridsClone = [];
+
+Array.from(choiceGrids).forEach((choiceGrid, index) => {
+  choiceGridsClone[index] = choiceGrid.cloneNode(true);
+  choiceGrid.addEventListener("click", function clickEvent(event) {
+    let finished = Object.values(record).every(bool => bool);
+    if (event.target === choiceGrid || finished) return;
+    
+    if (finished) {
+      render();
+    }
+
+    const allChoice = choiceGrid.getElementsByClassName("choice");
+
+    Array.from(allChoice).forEach((choice) => {
+      choice.classList.toggle("selected", false);
+      choice.classList.toggle("unchosen", true);
+      choice.childNodes[3].src = "images/unchecked.png";
+    });
+
+    const currentChoice = event.target.parentNode !== choiceGrid ? event.target.parentNode : event.target;
+
+    currentChoice.classList.toggle("selected");
+    currentChoice.classList.toggle("unchosen");
+    currentChoice.childNodes[3].src = "images/checked.png";
+    record[currentChoice.dataset.questionId] = true;
+
+    finished = Object.values(record).every(bool => bool);
+    if (finished) render();
+  });
+});
+
+function render() {
+  console.log("test");
+}
